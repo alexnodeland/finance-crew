d9f2b17f5d978a325bc2b808a7ef9c1ff153775f
Alex Nodeland <nodeland.alex@gmail.com>
2024-06-29 11:32:44 -0400

    refactor: move cli and split into multiple files

---
+++ src/finance_crew/cli/utils.py
@@ -0,0 +1,13 @@
+import time
+
+# ANSI escape codes for styling
+BOLD = "\033[1m"
+UNDERLINE = "\033[4m"
+BLUE = "\033[94m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+RED = "\033[91m"
+END = "\033[0m"
+
+def sleep(duration):
+    time.sleep(duration)
